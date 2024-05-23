e49698ef0e080503f0ef69d5fa0e7142e9222c10
Sergey Shapovalov <ssv@fozzy.com>
2024-05-23 22:23:31 +0200

    Fixed seed

--- db/seeds/posts.rb
+++ db/seeds/posts.rb
@@ -5,13 +5,13 @@ return if Post.any?
 posts = [
   {
     title: Faker::Lorem.sentence,
-    body: Faker::Lorem.paragraph,
+    body: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false),
     category: Category.first,
     creator: User.first
   },
   {
     title: Faker::Lorem.sentence,
-    body: Faker::Lorem.paragraph,
+    body: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false),
     category: Category.last,
     creator: User.last
   }
